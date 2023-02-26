e4e9d68ece95a22553be0bf90a995c2db1747d94
Stefano Delpero <stefano@delpero.eu>
2023-02-26 09:08:58 +0100

    Sample

---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor.Templating.Core;
+
+namespace WebApplication1.Pages;
+
+public class IndexModel : PageModel
+{
+    public string Html { get; set; } = string.Empty;
+
+    public async Task OnGetAsync()
+    {
+        Html = await RazorTemplateEngine.RenderAsync("Pages/Template.cshtml");
+    }
+}
---
+++ WebApplication1/WebRootPathTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApplication1;
+
+[HtmlTargetElement("webrootpath")]
+public class WebRootPathTagHelper : TagHelper
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public WebRootPathTagHelper(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.Content.AppendHtml(_environment.WebRootPath);
+    }
+}
